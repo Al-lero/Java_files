e6d629b6998cf2cc0369be9c5c86a9b52c0ebc7a
Al-lero <aleroutieyione@gmail.com>
2024-04-23 19:48:54 +0100

    SevenSegment

---
+++ SevenSegement/SevenSegment.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+	public class SevenSegment{
+
+		public static void main(String[] args){
+			Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter number to dispay: ");
+		int number = scanner.nextInt();
+
+
+	switch(number){
+
+		case 0:
+			System.out.println(" _\n| |\n|_|");
+		break;
+
+		case 1:
+			System.out.println(" | \n |   ");
+		break;   
+
+		case 2:
+			System.out.println(" _ \n _|\n|_");
+		break;
+
+		case 3:
+			System.out.println("_\n_|\n_|");
+		break;
+
+		case 4:
+			System.out.println("|  |\n _\n   |");
+		break;
+
+		case 5:
+			System.out.println(" _\n|_ \n _|");
+		break;
+
+		case 6:
+			System.out.println(" _\n|_\n|_|");
+		break;
+
+
+
+
+
+
+
+
+}
+
+}
+}
--- break/CheckOuts.java
+++ break/CheckOuts.java
@@ -1,59 +1,66 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-   	  public class CheckOut {
+	public class CheckOuts {
 		public static void main(String[] args){
 			Scanner scanner = new Scanner(System.in);
-			
-	
 
 String continueShopping;
-String customerName;
-String name;
-String buy;
-double pieces = 0;
-double unit = 0;
-double VAT = 17.50;
-int total = 0;
+String item;
+int quantity = 0;
+double price = 0;
+int subTotal = 0;
 double discount = 0;
-double subTotal = 0;
+double VAT = 17.50;
 double billTotal = 0;
 double balance = 0;
 int amountPaid = 0;
-
-               
-                System.out.println("Enter Customer's name: ");
-                	customerName = scanner.nextLine();
-		System.out.println("Cashier's name?:  ");
-			name = scanner.nextLine();
+double result = 0;
 		
 
+			ArrayList<Object> items = new ArrayList<>();
+
+		System.out.println("Enter Customer's name: ");
+		String customerName = scanner.nextLine();
+
+		System.out.println("Cashier's name: ");
+		String name = scanner.nextLine();
+
+
+		
 		do{
-		System.out.println("What do you want to buy[]? ");
-		buy = scanner.nextLine();
-		System.out.println("How many pieces[]?: ");
-		pieces = scanner.nextInt();
-		System.out.println("How much per unit[]?: ");
-		unit = scanner.nextInt();
-		subTotal += pieces * unit;
+		System.out.println("what do you want to buy: ");
+		item = scanner.nextLine();
+		
+		
+		System.out.println("How many pieces: ");
+		quantity = scanner.nextInt();
+		
+
+		System.out.println("How much per unit: ");
+		price = scanner.nextDouble();
+		
+
+                result = (quantity * price); 
+		subTotal += result;
 		discount = (subTotal * discount) / 100;
 		VAT = (subTotal * 17.50) / 100;
 		billTotal = subTotal + VAT - discount;
 		balance = amountPaid - billTotal;
+					
 		
-			
-
-		System.out.println("Do you want to buy something else[]? (yes/no)?");
+		System.out.println("Do you want to buy something else? (yes/no)?");
 		scanner.nextLine();
 		continueShopping = scanner.nextLine();
-		
+		items.add("\t" + item + "\t" + quantity + "\t" + price + "\t" + (quantity * price));
 
-		} while (continueShopping.equalsIgnoreCase("yes"));
+			}
+
+		while (continueShopping.equalsIgnoreCase("yes"));
 	
 			System.out.println("How much discount will he get?: ");
 			discount = scanner.nextDouble();
 			
-
-				
 			String prompt = """
 					WELCOME TO SEMICOLON STORE
 					MAIN BRANCH
@@ -61,13 +68,17 @@ int amountPaid = 0;
 					TEL:09014465195
 					
 					""";
+
+			
 			System.out.print(prompt);
 			System.out.println("Cashier's Name: "+ name);
 			System.out.println("Customer name: " + customerName);
 			System.out.println("====================================================");
 			System.out.println("\tITEM\tQTY\tPRICE\tTOTAL(NGN)");
 			System.out.println("\n---------------------------------------------------");
-			System.out.printf("\t" + buy + "\t" + pieces + "\t" + unit + "\t" + pieces * unit );
+			for (Object itemzz : items){
+			System.out.println(itemzz);
+			}
 			System.out.println("\n---------------------------------------------------");
 			System.out.printf("\nSubTotal: " + subTotal);
 			System.out.printf("\nDiscount: " + discount);
@@ -87,6 +98,9 @@ int amountPaid = 0;
 			
 
 			
-}
+
+
 
 }
+}
+	
