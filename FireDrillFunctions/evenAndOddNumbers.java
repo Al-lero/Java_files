03d66c99d51d7db3972e28dd39ea8179016ee529
Al-lero <aleroutieyione@gmail.com>
2024-04-24 06:15:38 +0100

    FireDrillFunctions

---
+++ FireDrillFunctions/evenAndOddNumbers.java
@@ -0,0 +1,24 @@
+
+	public static int evenAndOddNumber(int [] numbers){
+
+		int even = number[0];
+	for(int number = 0; number < numbers.length; number++){
+		if(number % 2 = 0){
+
+		System.out.println(numbers[counter] + " ");
+		}
+
+	return 0;
+	  }
+
+	for(int number = 0; number < numbers.length; number++){
+		if(number % 2 = 1){
+
+		System.out.println(numbers[] + " ");
+	   }
+
+	return 1;
+	}
+
+  }
+			
